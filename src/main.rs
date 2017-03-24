a3250293b0ef39a62456414f54228aaf85265dd6
Matthew Johnston <warmwaffles@gmail.com>
2017-03-23 20:24:28 -0500

    Take arguments to open a glTF file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 extern crate glium;
 
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use std::env;
+use std::process;
+
 use glium::DisplayBuild;
 use glium::Surface;
 
 fn main() {
+    env_logger::init().unwrap();
+
+    let file = env::args().nth(1);
+
+    if file.is_none() {
+        println!("Usage: gltf-viewer <path>");
+        println!("  path:   The path to the glTF file");
+        process::exit(1);
+    }
+
+    let file = file.unwrap();
+
+    info!("Opening {}", file);
+
     let display = glium::glutin::WindowBuilder::new()
         .with_dimensions(1024, 768)
         .with_title(format!("glTF Viewer"))
