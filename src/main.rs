96d406493ec02b2dac725b9507e6a5c118ec6f0a
Matthew Johnston <warmwaffles@gmail.com>
2017-03-23 20:10:50 -0500

    Open glium window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate glium;
+
+use glium::DisplayBuild;
+use glium::Surface;
+
 fn main() {
-    println!("Hello, world!");
+    let display = glium::glutin::WindowBuilder::new()
+        .with_dimensions(1024, 768)
+        .with_title(format!("glTF Viewer"))
+        .build_glium()
+        .unwrap();
+
+    loop {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+
+        target.finish().unwrap();
+
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return,
+                _ => ()
+            }
+        }
+    }
 }
